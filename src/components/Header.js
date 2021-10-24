45a4c71d011dea63d39a3931a7a8aa259f45cb61
Sergey <levashov04sergey@gmail.com>
2021-10-24 21:53:57 +0300

    create Header as func comp & AddContact as class comp

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>Contact Manager</h2>
+            </div>
         </div>
     );
 };
