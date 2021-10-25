7aca51312ef88a9e26fd1bdda545285a8a9bf25d
Sergey <levashov04sergey@gmail.com>
2021-10-25 22:47:12 +0300

    paste props from ContactCard to ContactDetail; add btn to ContactDetail using Link

--- src/components/ContactDetail.js
+++ src/components/ContactDetail.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import user from "../images/userOne.jpg";
 
 const ContactDetail = (props) => {
+    const {name, email} = props.location.state.contact
     return (
         <div className="main">
             <div className="ui card centered">
@@ -10,9 +11,16 @@ const ContactDetail = (props) => {
                     <img src={user} alt="user"/>
                 </div>
                 <div className="content">
-                    <div className="header">Gorgio</div>
-                    <div className="description">gg.gorgio@mail.com</div>
+                    <div className="header">{name}</div>
+                    <div className="description">{email}</div>
                 </div>
+            </div>
+            <div className="center-div">
+                <Link to="/">
+                    <button className="ui button blue center">
+                        Back to Contact List
+                    </button>
+                </Link>
             </div>
         </div>
     );
