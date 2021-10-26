e1af627e818d9b712221d6b212ee718d2bc20c74
Sergey <levashov04sergey@gmail.com>
2021-10-26 20:02:42 +0300

    create async fn for gettingdata from json-server;  put this data in state using useEffect

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import {v4 as uuid_v4} from 'uuid';
+import api from '../api/contacts';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -10,6 +11,12 @@ import ContactDetail from "./ContactDetail";
 function App() {
     const LOCAL_STORAGE_KEY = "contacts";
     const [contacts, setContacts] = useState([]);
+
+    //RetrieveContacts
+    const retrieveContacts = async () => {
+      const response = await api.get("/contacts");
+      return response.data;
+    };
 
     const addContactHandler = (contact) => {
         // console.log(contact);
@@ -26,8 +33,14 @@ function App() {
 
 
     useEffect(() => {
-        const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        if (retrieveContacts) setContacts(retrieveContacts);
+        // const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        // if (retrieveContacts) setContacts(retrieveContacts);
+        const getAllContacts = async () => {
+            const allContacts = await retrieveContacts();
+            if (allContacts) setContacts(allContacts);
+        };
+
+        getAllContacts();
     }, []);
 
     useEffect(() => {
